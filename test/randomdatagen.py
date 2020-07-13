ddfc1ec59ffba1b36150b004071ef0375e661bfd
thomas poudevigne-durance <drtrom95@gmail.com>
2020-07-13 16:55:51 +0100

    this randomly creates data

---
+++ test/randomdatagen.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from collections import namedtuple
+from random import uniform, randint
+import sqlalchemy as sa
+import pandas as pd
+
+def genrandom():
+    engine = sa.create_engine('sqlite:///flight.db')
+    connection = engine.connect()
+    Reading = namedtuple('Reading', 'flight, ts, temp, pressure, humidity, wind')
+    
+    
+    sql = """
+    CREATE TABLE readings (
+        flight    VARCHAR(10) NOT NULL,
+        ts        TIMESTAMP NOT NULL,
+        temp      NUMERIC(3,1) NOT NULL,
+        pressure  NUMERIC(4,0) NOT NULL,
+        humidity  NUMERIC(3,0) NOT NULL, 
+        wind    VARCHAR(10) NOT NULL,
+    
+        CONSTRAINT readings_pk PRIMARY KEY (flight, ts),
+        CONSTRAINT temp_ck CHECK (temp BETWEEN -70 AND 70),
+        CONSTRAINT pres_ck CHECK (pressure BETWEEN 0 AND 2000),
+        CONSTRAINT hum_ck CHECK (humidity BETWEEN 0 AND 100)
+    )
+    """
+    connection.execute(sql)
+    winds = ['strong','medium','weak']
+    readings = [
+        Reading(
+            flight=flights,
+            ts=f'2015-01-01 09:{str(i+1).zfill(2)}:00',
+            temp=round(uniform(23, 27), 1),
+            pressure=randint(1020, 1050),
+            humidity=randint(30, 50),
+            wind= winds[randint(0, 2)])
+        for flights in ['hab1', 'hab2', 'hab3']
+        for i in range(20)
+    ]
+    
+    sql = """
+        INSERT INTO readings
+            (flight, ts, temp, pressure, humidity, wind)
+        VALUES
+            (?, ?, ?, ?, ?, ?)
+    """
+    
+    for reading in readings:
+        values = (reading.flight, reading.ts, reading.temp, reading.pressure, reading.humidity,reading.wind)
+        connection.execute(sql, values)
+    print(pd.read_sql('readings',connection))
