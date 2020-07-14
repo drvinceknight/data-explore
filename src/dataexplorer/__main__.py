2aa089150e3384d305ae06bd76f2533fa12442a5
thomas poudevigne-durance <drtrom95@gmail.com>
2020-07-14 14:48:37 +0100

    new funtion: get database basic stats
    mean, std, maxi, mini, no_field, length

--- src/dataexplorer/__main__.py
+++ src/dataexplorer/__main__.py
@@ -7,9 +7,24 @@ def load_sql(file, table):
     """
     engine = sa.create_engine("sqlite:///" + file + ".db")
     connection = engine.connect()
+    database = pd.read_sql(table, connection)
+    return database
 
-def save_sql(file):
+def save_sql(df,file,table):
     """
-    Saves the data to an SQL table 
+    Saves the sql
     """
     engine = sa.create_engine("sqlite:///" + file + ".db")
+    df.to_sql(table, con=engine, if_exists="append")
+
+def get_stats(data):
+    """
+    provides the basic stats for the dataset
+    """
+    mean = data.mean()
+    std = data.std()
+    maxi = data.max()
+    mini = data.min()
+    no_field = len(data.columns)
+    length = len(data)
+    return [mean,std,maxi,mini,no_field,length]
