b0086ca1bfe4ebe56ca530f1db94b84497015358
Thomas Poudevigne <drtrom95@gmail.com>
2020-07-15 10:54:54 +0100

    handeling nonumeric data
    such as time data,catgorical data, with a both extrating function and
    and puting back in place

--- src/dataexplorer/__main__.py
+++ src/dataexplorer/__main__.py
@@ -28,3 +28,38 @@ def get_stats(data):
     no_field = len(data.columns)
     length = len(data)
     return [mean,std,maxi,mini,no_field,length]
+
+def categorical_handle(data):
+    """
+    Transforms categorical data into numerical, saves maping on a list.
+    """
+    indexs = []
+    for name in data.columns:
+        if "O" == data[name].dtype:
+            new = pd.factorize(data[name])
+            data[name] = new[0]
+            indexs.append(new[1])
+    return data, indexs
+
+def collect_column(data):
+    return data.columns
+
+def place_columns(database,col):
+    database.columns = col
+    return database
+
+def place_categorical(index,data,cat):
+    line = data[cat].round()
+    for i in range(len(index)):
+        line = line.replace(i,index[i])
+    data[cat] = line
+    return data
+
+def timestamp_handle(times):
+    m = times.minute
+    hh = times.hour*100
+    dd = times.day*10000
+    mm = times.month*1000000
+    yyyy = times.year*10000000
+    numtime = m + hh + dd + mm + yyyy
+    return numtime
