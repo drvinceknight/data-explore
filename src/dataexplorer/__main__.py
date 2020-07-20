bb5a6359d9c1fa4381298a1ba38bb90967ef010c
thomas poudevigne-durance <drtrom95@gmail.com>
2020-07-20 11:07:09 +0100

    time_handle better and added reveses
    the new vertion funtion now uses the build in timestamp() funtion that convert m8[ns] datatype to int
    rather the add up system
    
    add funtion that reverses both catigorical and timebase trasfomations

--- src/dataexplorer/__main__.py
+++ src/dataexplorer/__main__.py
@@ -10,12 +10,14 @@ def load_sql(file, table):
     database = pd.read_sql(table, connection)
     return database
 
-def save_sql(df,file,table):
+
+def save_sql(df, file, table):
     """
     Saves the sql
     """
     engine = sa.create_engine("sqlite:///" + file + ".db")
     df.to_sql(table, con=engine, if_exists="append")
+
 
 def get_stats(data):
     """
@@ -27,11 +29,13 @@ def get_stats(data):
     mini = data.min()
     no_field = len(data.columns)
     length = len(data)
-    return [mean,std,maxi,mini,no_field,length]
+    return [mean, std, maxi, mini, no_field, length]
+
 
 def categorical_handle(data):
     """
-    Transforms categorical data into numerical, saves maping on a list.
+    transforms categorical data into numerical, saves maping on a list.
+    this calls timestamp_handle fuction
     """
     indexs = []
     for name in data.columns:
@@ -39,27 +43,65 @@ def categorical_handle(data):
             new = pd.factorize(data[name])
             data[name] = new[0]
             indexs.append(new[1])
+        elif "<M8[ns]" == data[name].dtype:
+            times = timestamp_handle(data[name])
+            data[name] = times
+            indexs.append(["time"])
+        else:
+            indexs.append(["#"])
     return data, indexs
 
+
+def place_time(data, cat):
+    """
+    transform time from numerical back to ctime
+    """
+    line = data[cat]
+    fulltime = []
+    for l in line:
+        print(l)
+        fulltime.append(time.ctime(l))
+    data[cat] = fulltime
+    return data
+
+
 def collect_column(data):
+    """
+    collects the column name
+    """
     return data.columns
 
-def place_columns(database,col):
+
+def place_columns(database, col):
+    """
+    insert column name to the dataset
+    """
     database.columns = col
     return database
 
-def place_categorical(index,data,cat):
+
+def place_categorical(index, data, cat):
+    """
+    categorize numerical values based on the index list
+    """
     line = data[cat].round()
     for i in range(len(index)):
-        line = line.replace(i,index[i])
+        try:
+            line = line.replace(i, index[i])
+        except IndexError:
+            line = line.replace(i, None)
     data[cat] = line
     return data
 
+
 def timestamp_handle(times):
-    m = times.minute
-    hh = times.hour*100
-    dd = times.day*10000
-    mm = times.month*1000000
-    yyyy = times.year*10000000
-    numtime = m + hh + dd + mm + yyyy
+    """
+    covert time into numrical this can be revert be time.ctime()
+    """
+    numtime = []
+    for t in times:
+        numtime.append(t.timestamp())
     return numtime
+
+def main(file, table, ouptut):
+    pass
