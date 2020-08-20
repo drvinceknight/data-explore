fb5658024b6ab2bd78445a21c6713192325197b4
thomas poudevigne-durance <drtrom95@gmail.com>
2020-08-20 15:15:43 +0100

    add test for the genrated dataset

---
+++ test/test_data_gen.py
@@ -0,0 +1,8 @@
+from randomdatagen import generate_random_testing_data
+
+def test_generate_random_testing_data():
+    """
+    create and verify a randomly gentrated testing dataset
+    """
+    data = generate_random_testing_data(20)
+    assert len(data) == 60
