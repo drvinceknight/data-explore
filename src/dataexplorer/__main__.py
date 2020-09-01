ec387cbe937b24a94a20bbea3bd89a8030bc48fe
thomas poudevigne-durance <drtrom95@gmail.com>
2020-09-01 16:28:24 +0100

    can now read mutiple file types

--- src/dataexplorer/__main__.py
+++ src/dataexplorer/__main__.py
@@ -7,14 +7,45 @@ import logging
 @click.option("--target", help="the file that you want to import")
 @click.option("--table", help="what table you are using")
 @click.option("--output", help="the file that you are saving as")
+@click.option("--convert",help="the file extention you like to convert to")
 
-def main(target, table, output):
+def main(target, table, output,convert):
     """
     read data and logs it
     """
-    data = load_sql(target,table)
+    name,extention = target.split('.')
+    if extention == 'db':
+        data = load_sql(name,table)
+    elif extention == 'csv':
+        data = pandas.read_csv(target)
+    elif extention == 'json':
+        data = pandas.read_json(target)
+    elif extention == 'sav':
+        data = pandas.read_spss(target)
+    else:
+        return None#raise Error("file type not supported")
     stats = get_stats(data)
     make_log_file(stats, output)
+    save_file(data,convert)
+
+def save_file(data,convert,name):
+    if convert == 'db':
+        save_sql(data,name,'table')
+    elif convert == 'csv':
+        pass
+    elif convert == 'json':
+        pass
+    elif convert == 'sav':
+        pass
+    else:
+        return None
+
+def save_sql(df,file,table):
+    """
+    Saves the generated data to a SQL table called generated_data
+    """
+    engine = sa.create_engine("sqlite:///" + file + ".db")
+    df.to_sql(table, con=engine, if_exists="append")
 
 def load_sql(file, table):
     """
