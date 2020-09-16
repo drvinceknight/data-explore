7d7bddd9f8918a902f31857b0675865f61045cb3
Vince Knight <vincent.knight@gmail.com>
2020-09-16 14:50:06 +0100

    Add dependencies to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+# Read in the requirements.txt file
+with open("requirements.txt") as f:
+    requirements = []
+    for library in f.read().splitlines():
+        requirements.append(library)
+
 setup(
     name="dataexplorer",
     version="0.0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    install_requires=requirements,
 )
